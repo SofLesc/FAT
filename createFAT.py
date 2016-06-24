0e072fe5a5d91fba34aa237ce05c1ffd92a8dee8
Sofia Lescano <sofia.lescano@lnls.br>
2016-06-24 12:07:43 -0300

    Folders and subfolders implemented

--- createFAT.py
+++ createFAT.py
@@ -3,15 +3,88 @@ import sys
 import getopt
 import os
 
+output = 'output.txt'
+output_file = open(output, "w")
+cluster = 0
+directory_number = 0
+# 300 = 100 caracteres
+cluster_size = 300
+ofset = 300
+file_number = 0
+
+# Nome do arquivo em 8 bytes
+# Extensao do arquivo em 3 bytes
+# Cluster de dados em 1 byte (endereco dos dados) ou numero de pasta
+# Tamanho do arquivo em 2 bytes
+# Numero de pasta onde se encontra 2 bytes
+
+#Exemplo
+#n = numero de arquivo ou pasta
+#p = parent directory
+#    name               |   ext  |n | size| p
+#74 65 73 74 20 20 20 20 74 78 74 00 00 05 00 00
+#t  e  s  t  20 20 20 20 t  x  t  00 00 05 00 00
+
+#70 61 73 74 61 20 20 20 20 20 20 01 00 00 00 00
+#p  a  s  t  a  20 20 20 20 20 20 01 00 00 00 00
+
+#Analisa as pastas e subpastas
+def parseDirectory (directory, parentDirectory):
+	global cluster
+	global directory_number
+	global file_number
+	for filename in os.listdir(directory):
+		count = 0
+		if(os.path.isdir(directory + '/' + filename) == 1):
+			for c in filename:
+				output_file.write(c.encode("hex") +' ')
+				#output_file.write(c +'  ')
+			for i in range(11 - len(filename)):
+				output_file.write('20 ')
+			directory_number += 1
+			output_file.write("%02d" % (directory_number,) + ' ')
+			output_file.write('00 00 ')
+			output_file.write('00 ')
+			output_file.write("%02d" % (parentDirectory,) + '\n')
+			print filename + " is a directory"
+			file_number += 1
+			os.chdir(directory)
+			parseDirectory(filename, directory_number)
+			os.chdir("..")
+		else:
+			print filename			
+			for c in filename:
+				if hex(ord(c)) != '0x2e':
+					output_file.write(c.encode("hex") +' ')
+					#output_file.write(c +'  ')
+					count = count + 1
+				else:
+					diff = 8 - count
+					for i in range(diff):
+						output_file.write('20 ')
+			output_file.write("%02d" % (cluster,) + ' ')
+			output_file.seek(ofset + cluster*cluster_size)
+			current_file = open(directory + '/' + filename)
+			size = 0	
+			#TODO copy in binary not hexa	
+			for line in current_file:
+				for c in line:
+					output_file.write(c.encode("hex") +'\n')
+					size = size + 1	
+			output_file.seek(file_number*16*3 + 3*12)
+			cluster = cluster + 1
+			file_number += 1
+			high = size/256
+			output_file.write("%02x" % (high,) + ' ')
+			low = size - 256*high
+			output_file.write("%02x" % (low,) + ' ')
+			output_file.write('00 ')
+			output_file.write("%02d" % (parentDirectory,) + '\n')
+			
+
 def main(argv):
 	
 	directory = ''
-	output = 'output.txt'
-	output_file = open(output, "w")
-	cluster = 0
-	# 300 = 100 caracteres
-	cluster_size = 300
-	ofset = 300
 	
 	try:
 		opts, args = getopt.getopt(argv,"hd:",["directory"])
@@ -24,40 +97,17 @@ def main(argv):
 			sys.exit()
 		elif opt in ("-d", "--directory"):
 			directory = arg
-	print 'Directory is', directory
+	print 'Directory to analyse: ', directory
 	
 	#Deixa espaco para escrever no arquivo	
 	for i in range(1000):		
 		output_file.write('  \n')
 	output_file.seek(0)	
 	
-	number_of_files = len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
-	output_file.write("%02d" % (number_of_files,) + '\n')
+	#number_of_files = len([name for name in os.listdir(directory)])
+	#output_file.write("%02d" % (number_of_files,) + '\n')
 		
-
-	for filename in os.listdir(directory):
-		count = 0
-		for c in filename:
-			if hex(ord(c)) != '0x2e':
-				output_file.write(c.encode("hex") +'\n')
-				count = count + 1
-			else:
-				diff = 8 - count
-				for i in range(diff):
-					output_file.write('20\n')
-		output_file.write("%02d" % (cluster,) + '\n')
-		#output_file.seek(ofset + cluster*cluster_size)
-		output_file.seek(ofset + cluster*cluster_size)
-		current_file = open(directory + '/' + filename)
-		size = 0		
-		for line in current_file:
-			for c in line:
-				output_file.write(c.encode("hex") +'\n')
-				size = size + 1	
-		cluster = cluster + 1
-		output_file.seek(cluster*13*3)
-		output_file.write("%02d" % (size,) + '\n')
-			
+	parseDirectory(directory, 00)
 			
 if __name__ == "__main__":
    main(sys.argv[1:])
--- readFAT.py
+++ readFAT.py
@@ -3,6 +3,22 @@ import sys
 import getopt
 import os
 
+# Nome do arquivo em 8 bytes
+# Extensao do arquivo em 3 bytes
+# Cluster de dados em 1 byte (endereco dos dados) ou numero de pasta
+# Tamanho do arquivo em 2 bytes
+# Numero de pasta onde se encontra 2 bytes
+
+#Exemplo
+#n = numero de arquivo ou pasta
+#p = parent directory
+#    name               |   ext  |n | size| p
+#74 65 73 74 20 20 20 20 74 78 74 00 00 05 00 00
+#t  e  s  t  20 20 20 20 t  x  t  00 00 05 00 00
+
+#70 61 73 74 61 20 20 20 20 20 20 01 00 00 00 00
+#p  a  s  t  a  20 20 20 20 20 20 01 00 00 00 00
+
 def main(argv):
 	
 	directory = 'readtest'
@@ -10,6 +26,8 @@ def main(argv):
 	# 300 = 100 caracteres
 	cluster_size = 300
 	ofset = 300
+	pastas = {}
+	pastas[0] = directory
 	
 	try:
 		opts, args = getopt.getopt(argv,"hi:",["input_name"])
@@ -24,16 +42,76 @@ def main(argv):
 			input_name = arg
 	input_file = open(input_name)	
 	
-	os.mkdir(directory, 0755)
-	os.chdir(directory)
+	if(os.path.isdir(directory) == 0):	
+		os.mkdir(directory, 0755)
+	#os.chdir(directory)
 	
 	file_name = ''	
 	count = 0
-	first = 1
 	number_of_files = 0
 	file_number = 0
+	directory = False
+	parent_directory = 0
+	file_number = 0
+	
 
 	for line in input_file:
+		start = 0
+		end = 2
+		first = True
+
+		#File name
+		for i in range (8):
+			aux = int(line[start:end], 16)
+			if(aux > 33):
+				file_name = file_name + line[start:end].decode('hex')
+			start +=3
+			end += 3
+
+		#File extension		
+		for i in range (3):
+			aux = int(line[start:end], 16)
+			if(aux > 33):
+				if(first):
+					file_name += "."
+					first = False
+				file_name = file_name + line[start:end].decode('hex')
+			else:
+				directory = True
+			start +=3
+			end += 3
+		print file_name
+		if (directory):
+			file_number += 1
+			directory = False
+			directory_number = int(line[start:end], 16)
+			parent_directory = int(line[-3:-1], 16)
+			os.mkdir(pastas[parent_directory] + "/" + file_name)
+			pastas[directory_number] = pastas[parent_directory] + "/" + file_name
+			file_name = ''
+			continue
+		else:
+			file_number += 1
+			parent_directory = int(line[-3:-1], 16)
+			new_file = open(pastas[parent_directory] + "/" + file_name, "w")
+			cluster = int(line[start:end], 16)
+			start +=6
+			end += 6
+			# TODO Pegar os dois bits
+			size = int(line[start:end], 16)
+			file_name = ''
+			input_file.seek(ofset + cluster*cluster_size)
+			line_number = 1	
+			content = ''		
+			for line in input_file:
+				if line_number > size:
+					break
+				content = content + line[0:-1].decode('hex')
+				line_number += 1
+			input_file.seek((file_number)*16*3)
+			new_file.write(content)
+		#break
+'''
 		count = count + 1
 		if(first):
 			count = 0
@@ -46,30 +124,44 @@ def main(argv):
 				file_name = file_name + '.'
 			#File extension		
 			if count >= 9 and count < 12:
-				file_name = file_name + line[0:-1].decode('hex')
-			if count == 11:	
-				print file_name	
-				new_file = open(file_name, "w")
-				file_name = ''
-			if count == 12:
-				 cluster = int(line)
-			if count == 13:
-				count = 0
-				size = int(line)
-				#data
-				input_file.seek(ofset + cluster*cluster_size)
-				line_number = 1	
-				content = ''		
-				for line in input_file:
-					if line_number > size:
+				if int(line[0:-1]) < 21 and count == 9:
+					directory = 1
+					file_name = file_name[0:-1]
+				else:	
+					file_name = file_name + line[0:-1].decode('hex')	
+			if directory:				
+				if count == 13:
+					print file_name
+					count = 0
+					os.mkdir(file_name)
+					directory = 0
+					file_number = file_number + 1
+					if(file_number == number_of_files):
 						break
-					content = content + line[0:-1].decode('hex')
-					line_number = line_number + 1
-				input_file.seek((cluster + 1)*14*3)
-				new_file.write(content)
-				file_number = file_number + 1
-				if(file_number == number_of_files):
-					break
-
+			else:			
+				if count == 11:	
+					print file_name	
+					new_file = open(file_name, "w")
+					file_name = ''
+				if count == 12:
+					 cluster = int(line)
+				if count == 13:
+					count = 0
+					size = int(line)
+					#data
+					input_file.seek(ofset + cluster*cluster_size)
+					line_number = 1	
+					content = ''		
+					for line in input_file:
+						if line_number > size:
+							break
+						content = content + line[0:-1].decode('hex')
+						line_number = line_number + 1
+					input_file.seek((cluster + 1)*13*3 + 3)
+					new_file.write(content)
+					file_number = file_number + 1
+					if(file_number == number_of_files):
+						break
+'''
 if __name__ == "__main__":
    main(sys.argv[1:])
